81b970576c96fcc4318dbc91a3f259ecf1ec5ea7
hamah99 <steve@maclloyd.com>
2024-02-05 22:39:55 -0800

    Updated February  5, 2024 10:39pm

--- db_dump.sql
+++ db_dump.sql
@@ -39,7 +39,7 @@ CREATE TABLE adlist
 	invalid_domains INTEGER NOT NULL DEFAULT 0,
 	status INTEGER NOT NULL DEFAULT 0
 );
-INSERT INTO adlist VALUES(1,'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',1,1706559061,1706559061,'Migrated from /etc/pihole/adlists.list',1707048063,158823,1,2);
+INSERT INTO adlist VALUES(1,'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',1,1706559061,1706559061,'Migrated from /etc/pihole/adlists.list',1707199832,158823,1,2);
 CREATE TABLE domainlist_by_group
 (
 	domainlist_id INTEGER NOT NULL REFERENCES domainlist (id),
